2ba55dcc4b9115262f21f612f5414bb9b7d82285
Tenrah <frd.frenaud@gmail.com>
2020-08-06 22:39:15 +0200

    Update Auth0 config with env variables

--- src/utils/auth0.ts
+++ src/utils/auth0.ts
@@ -5,9 +5,10 @@ const config: IAuth0Settings = {
     domain: process.env.AUTH0_DOMAIN || '',
     clientId: process.env.AUTH0_CLIENT_ID || '',
     clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
+    audience: process.env.AUD,
     scope: 'openid profile',
-    redirectUri: 'http://localhost:3000/api/callback',
-    postLogoutRedirectUri: 'http://localhost:3000/',
+    redirectUri: process.env.REDIRECT_URI_LOGIN || 'http://localhost:3000/api/callback',
+    postLogoutRedirectUri: process.env.REDIRECT_URI_LOGOUT || 'http://localhost:3000',
     session: {
         cookieSecret: process.env.COOKIE_SECRET || '',
         cookieLifetime: 60 * 60 * 8,
